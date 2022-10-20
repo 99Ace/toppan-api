55aa921123ec0b6d8aa561b9be324d66a90633b7
99Ace <ace.ang@trentglobal.edu.sg>
2022-10-20 22:49:32 +0000

    setup routes for api

--- index.js
+++ index.js
@@ -13,8 +13,25 @@ const main = async () => {
   app.get("/", async (req, res) => {
     res.send("Welcome to 99Ace World");
   });
-
-  
+  // 1. REGISTER ROUTE : register one or more students to a specified teacher
+  app.post("/api/register", async (req, res) => {
+    res.send("Route 1: register one or more students to a specified teacher");
+  });
+  // 2. READ ROUTE : retrieve a list of students common to a given list of teachers
+  app.get("/api/commonstudents", async (req, res) => {
+    res.send(
+      "Route 2: retrieve a list of students common to a given list of teachers"
+    );
+  });
+  // 3. SUSPEND STUDENT ROUTE : suspend a specified student
+  app.post("/api/suspend", (req, res) => {
+    // res.sendStatus(204);
+    res.send("Route 3: Suspend Student [POST]");
+  });
+  //4. ROUTE : retrieve a list of students who can receive a given notification
+  app.post("/api/retrievefornotifications", (req, res) => {
+    res.send("Route 4: Retrieve Notifcation");
+  });
 };
 main();
 app.listen(3000, () => {
